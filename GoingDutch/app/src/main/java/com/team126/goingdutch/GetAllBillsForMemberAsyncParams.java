614b739185a370468717ac845b44227985538f16
Radheshyam B. Soni <radheshyambsoni@gmail.com>
2022-11-20 14:45:55 +0530

    Adding the complete project to github

---
+++ GoingDutch/app/src/main/java/com/team126/goingdutch/GetAllBillsForMemberAsyncParams.java
@@ -0,0 +1,12 @@
+package com.team126.goingdutch;
+
+class GetAllBillsForMemberAsyncParams {
+
+    String gName;
+    int mid;
+
+    GetAllBillsForMemberAsyncParams(String gName, int mid) {
+        this.gName = gName;
+        this.mid = mid;
+    }
+}
